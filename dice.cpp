bcd8bd12981e4b6fcbdc7614febef45e753797b3
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-01-19 00:33:29 -0500

    Create dice.cpp

---
+++ dice.cpp
@@ -0,0 +1,27 @@
+#include "dice.hpp"
+
+Dice::Dice(int n){
+    nDice = n;
+    diceVal = new int [nDice];
+    srand(time(NULL));
+
+} //constructor
+
+Dice::~Dice() {
+    delete [] diceVal;
+} //destructor
+
+const int* Dice:: roll () {
+    for (int j = 0; j < nDice; j++) {
+        diceVal [j] = rand();
+    }
+    return diceVal;
+}
+
+ostream& Dice::print(ostream& diceOutput) {
+    for (int j = 0; j < nDice; j++) {
+        diceOutput << diceVal [j];
+    }
+    return diceOutput;
+
+}
