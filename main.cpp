24fd8cd2d3d11261b9bb880320d052aaebdc1904
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-02-01 20:28:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,32 +5,29 @@
 
 
 void UnitDice (){
-  srand(time(nullptr)); // random number each time
-  
-  // Testing all numbers from 1 to 10 
-int nDice = rand() % 10 + 1  ; 
-    banner() ;
+    srand(time(nullptr)); // random number each time
+
+    // Testing all numbers from 1 to 10
+    int nDice = rand() % 10 + 1  ;
+    banner();
     ofstream
-    Diceroll("Diceroll.txt",ios::app);
+            Diceroll("Diceroll.txt",ios::app);
+
+    /*Output if file can't open*/
+    if (!Diceroll.is_open()) {
+        fatal ("File Diceroll.txt cannot be opened for writing.");
+    }
 
     Dice rolling(nDice);
     Diceroll << "And your rolls are:" << endl;
-    rolling.roll();
-    rolling.print(Diceroll);
+    const int* Rollnum = rolling.roll(); //this helps to access the array
+    rolling.print(Diceroll); //calling the print function
+    cout << "And your rolls are:\n" << rolling << endl;
     Diceroll << endl;
     Diceroll.close();
 }
 int main() {
-
-  /*Sending out to the file*/
-    std::ofstream outputFile("Diceroll.txt",
-    ios::app);
-    /*Output if file can't open*/
-    if (!outputFile.is_open()) {
-       void fatal();
-    }
     UnitDice();
-    outputFile.close();
     bye();
     return 0;
 }
